567556877ad1f314b7ba14ff8beeae815d176fb4
fletcher-greg <fletcher.web.dev.one@gmail.com>
2019-07-01 15:30:26 +0200

    Updates

---
+++ precache-manifest.9ff9b073c7517699ab0e5e97ce31b5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e9ba8e4d968d5e5f4dc8b4178ead5b",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "5b0d8c9bed0198cdd831",
+    "url": "/random-quote-machine/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "c10e1e299d99985f7e3d",
+    "url": "/random-quote-machine/static/css/main.558887d7.chunk.css"
+  },
+  {
+    "revision": "5b0d8c9bed0198cdd831",
+    "url": "/random-quote-machine/static/js/2.3024b44a.chunk.js"
+  },
+  {
+    "revision": "c10e1e299d99985f7e3d",
+    "url": "/random-quote-machine/static/js/main.4bd469c9.chunk.js"
+  },
+  {
+    "revision": "6fefced6fd426e504746",
+    "url": "/random-quote-machine/static/js/runtime~main.296aa1cf.js"
+  }
+]);
